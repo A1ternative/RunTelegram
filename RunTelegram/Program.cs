71f7494453372c1668618d823a85cf82ac5a0ec3
A1ternative <alternative6556@gmail.com>
2021-02-26 10:11:29 +0400

    Add repos

---
+++ RunTelegram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RunTelegram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string tgPath = @"C:\Users\Admin\TgPaths.txt";
+			string[] readTgPath = File.ReadAllLines(tgPath);
+			string tgExe = @"Telegram.exe";
+			string combinePath;
+
+			foreach (string s in readTgPath)
+			{
+				combinePath = Path.Combine(s, tgExe);
+				Process.Start(combinePath);
+			}
+			//
+		}
+	}
+}
