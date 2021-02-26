45b291f57eab089ad43efedff0504c551a991c9b
A1ternative <alternative6556@gmail.com>
2021-02-27 00:49:19 +0400

    Rewrite with functions

--- RunTelegram/Program.cs
+++ RunTelegram/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace RunTelegram
 {
@@ -9,17 +11,36 @@ namespace RunTelegram
 	{
 		static void Main(string[] args)
 		{
-			string tgPath = @"C:\Users\Admin\TgPaths.txt";
-			string[] readTgPath = File.ReadAllLines(tgPath);
-			string tgExe = @"Telegram.exe";
-			string combinePath;
+			Console.WriteLine("Укажи путь до файлика с путями телег. Путь и файл не должны соджержать пробелы!!!");
+			Console.WriteLine("Пример содержимого файлика:");
+			Console.WriteLine(@"D:\Telegrams\15901");
+			Console.WriteLine(@"D:\Telegrams\15902");
+			Console.WriteLine(@"");
+			Console.WriteLine(@"Для остановки программы нажми Ctrl+C в этом окне");
+			RunAllTelegrams(ReadPathsFromFile());
 
-			foreach (string s in readTgPath)
+			static List<string> ReadPathsFromFile()
 			{
-				combinePath = Path.Combine(s, tgExe);
-				Process.Start(combinePath);
+				string fileWithPaths = Console.ReadLine();
+				string[] readTgPath = File.ReadAllLines(@fileWithPaths);
+				string tgExe = @"Telegram.exe";
+				List <string> PathToTgExe = new List<string>();
+
+				foreach (string path in readTgPath)
+				{
+					PathToTgExe.Add(Path.Combine(path, tgExe));
+				}
+				return PathToTgExe;
 			}
-			//
+
+			static void RunAllTelegrams(List<string> Paths)
+			{
+				foreach (string path in Paths)
+				{
+					Process.Start(path);
+					Thread.Sleep(5000);
+				}
+			}
 		}
 	}
 }
